d54c124d4916d5b81a77ac04958155d402462a6f
Vasyl Glodan <glodanif@gmail.com>
2017-08-29 21:48:42 +0300

    Generating of output archive name

--- pack.bat
+++ pack.bat
@@ -1,36 +1,27 @@
-rem for /d %%X in (*) do "c:\Program Files\7-Zip\7z.exe" a -tzip "CombatIndicator.zip" "Images" "Sounds"
-
 @echo off
-setlocal
-
-set "pattern=Version"
-set "SEARCHTEXT=## Version: v"
-set "REPLACETEXT="
 
 for /f "tokens=*" %%a in (CombatIndicator.toc) do (
-
-        set "line=%%a"
-        setlocal enabledelayedexpansion
-        if not "!line:%pattern%=!"=="!line!" (
-            echo %%a
-
-            SET string=%%a
-            SET modified=!string:%SEARCHTEXT%=%REPLACETEXT%!
-
-            echo !modified!
-        )
-        endlocal
-
+    set LINE=%%a
+    setlocal enabledelayedexpansion
+    if not "!LINE:Version=!"=="!LINE!" (
+        set "ARCHIVE_NAME=CombatIndicator !LINE:## Version: =!.zip"
+        goto :zip
+    )
+    endlocal
 )
+
+:zip
+echo %ARCHIVE_NAME%
+
+if not exist output\CombatIndicator mkdir output\CombatIndicator
 
 set "IGNORED = .gitignore .git .idea"
 set LIST = for %%x in (*) do set LIST=!LIST! %%x
 
-(
- for %%i in (%LIST%) do (
-  echo(%%i
-  echo(
- )
+echo %LIST%
+
+for %%i in (%LIST%) do (
+    echo %%i
 )
 
-@rem "c:\Program Files\7-Zip\7z.exe" a -tzip "CombatIndicator.zip" %LIST%
+@rem "c:\Program Files\7-Zip\7z.exe" a -tzip %ARCHIVE_NAME% %LIST%
